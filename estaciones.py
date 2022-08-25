17431622a8c4fa17174f661420a0dcdd1caabdbe
esteban palencia <eteban28@gmail.com>
2022-08-24 23:06:01 -0500

    NTJ3: se crean condicionales simples y anidados

---
+++ estaciones.py
@@ -0,0 +1,29 @@
+
+Mes = input("Digite el mes del año: ")
+
+if Mes == 'enero':
+    print("Estamos en Invierno")
+elif Mes == "febrero":
+    print("Estamos en Invierno")
+elif Mes == "marzo":
+    print("Estamos en Invierno")
+elif Mes == "abril":
+    print("Estamos en Primavera")
+elif Mes == "mayo":
+    print("Estamos en Primavera")
+elif Mes == "junio":
+    print("Estamos en Primavera")
+elif Mes == "julio":
+    print("Estamos en Verano")
+elif Mes == "agosto":
+    print("Estamos en Verano")
+elif Mes == "septiembre":
+    print("Estamos en Verano")
+elif Mes == "octubre":
+    print("Estamos en Winter")
+elif Mes == "noviembre":
+    print("Estamos en Winter")
+elif Mes == "diciembre":
+    print("Estamos en Winter")
+else:
+    print("el mes digitado no es valido")
